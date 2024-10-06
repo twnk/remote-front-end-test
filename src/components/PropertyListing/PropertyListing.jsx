f55913de35b285e10eb46a55fcc24854040ea345
Angel Wells <angel.wells@immediate.co.uk>
2024-10-06 10:42:54 +0100

    feat: fetch properties on load

--- src/components/PropertyListing/PropertyListing.jsx
+++ src/components/PropertyListing/PropertyListing.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetch } from 'cross-fetch';
 import PropertyCard from '../PropertyCard';
 import './PropertyListing.scss';
 
-const DUMMY_PROPERTY = {
-    id: 73864112,
-    bedrooms: 3,
-    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
-    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
-    propertyType: 'Flat',
-    price: 1950000,
-    branchName: 'M2 Property, London',
-    propertyUrl: '/property-for-sale/property-73864112.html',
-    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
-    propertyTitle: '3 bedroom flat for sale',
-    mainImage:
-        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
-};
-
 const PropertyListing = () => {
+    const [properties, setProperties] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('http://localhost:3000/api/properties');
+
+            if (!res.ok) {
+                throw new Error('Failed to fetch data', { status: res.status, message: res.message });
+            }
+
+            const json = await res.json();
+
+            return json;
+        };
+
+        fetchData().then((data) => setProperties(data));
+    }, []);
+
     return (
         <ul className="PropertyListing">
-            {Array(5)
-                .fill(DUMMY_PROPERTY)
-                .map((property, index) => (
-                    <li key={index}>
-                        <PropertyCard {...property} />
-                    </li>
-                ))}
+            {properties.map((property, index) => (
+                <li key={index}>
+                    <PropertyCard {...property} />
+                </li>
+            ))}
         </ul>
     );
 };
