cc80cf37eba3f12737debc268ece457e701225a4
Angel Wells <angel.wells@immediate.co.uk>
2024-10-06 10:49:16 +0100

    refactor: use mock server and fix test failure

--- src/components/PropertyListing/tests/PropertyListing.spec.jsx
+++ src/components/PropertyListing/tests/PropertyListing.spec.jsx
@@ -3,11 +3,21 @@ import { render, screen } from '@testing-library/react';
 import { within } from '@testing-library/dom';
 import PropertyListing from '../PropertyListing';
 
+jest.mock('cross-fetch', () => {
+    const crossFetch = jest.requireActual('cross-fetch');
+    const { mockFetch } = jest.requireActual('../../../mockFetch');
+
+    return {
+        ...crossFetch,
+        fetch: jest.fn(mockFetch),
+    };
+});
+
 describe('PropertyListing', () => {
-    it('should render five property cards', async () => {
+    it('should render 101 property cards', async () => {
         render(<PropertyListing />);
         const propertiesList = screen.getByRole('list');
         const propertyCards = await within(propertiesList).findAllByRole('listitem');
-        expect(propertyCards).toHaveLength(5);
+        expect(propertyCards).toHaveLength(101);
     });
 });
